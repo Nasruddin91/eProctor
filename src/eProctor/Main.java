295c934ed86803e41355f4a553b13b1be3872494
Nasruddin91 <nasruddin_work@hotmail.com>
2014-02-27 14:36:58 +0800

    hey

--- src/eProctor/Main.java
+++ src/eProctor/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Just testing!!!!!");
+		System.out.println("Don't forget to fork!!!");
 	}
 
 }
